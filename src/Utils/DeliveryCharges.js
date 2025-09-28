05146fbd995bb916755fa8034993e603f56ece43
ashu <ashutoshrajput280@gmail.com>
2025-09-28 15:32:58 +0530

    ok\

--- src/Utils/DeliveryCharges.js
+++ src/Utils/DeliveryCharges.js
@@ -2,10 +2,10 @@ function calculateDeliveryCharges(area = "") {
   const charges = {
     madhuban: 20,
     colony: 20,
-    daha: 30,
-    bazida: 50,
-    "uncha smana": 50,
-    "arpana hospital": 50,
+    daha: 20,
+    bazida: 30,
+    "uncha smana": 30,
+    "arpana hospital": 30,
   };
   const key = area.trim().toLowerCase();
   return charges[key] || 0;
